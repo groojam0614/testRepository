b79c27b127720553a978c3eac898e7ae75dafaba
groojam0614 <ayfls0614@naver.com>
2016-03-30 23:32:40 +0900

    aaaa

--- test/src/main/java/main.java
+++ test/src/main/java/main.java
@@ -4,22 +4,14 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test2 test = new test2();
 		
 		System.out.print("Hello Word!");
-		
-		
-		
-		System.out.print(test2.add());
+		System.out.println("future");
 	}
 	
 
 }
-class test2{
-	public static int add(){
-		return 7 + 8;
-	}
-}
+
 	
 	
 
