954f97b17adfc01183bde283f9cb5e9bb6bdea77
Richsea <201402377@cs-cnu.org>
2016-03-30 22:34:02 +0900

    test

---
+++ test/src/main/java/main.java
@@ -0,0 +1,26 @@
+package main.java;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test2 test = new test2();
+		
+		System.out.print("Hello Word!");
+		
+		
+		
+		System.out.print(test2.add());
+	}
+	
+
+}
+class test2{
+	public static int add(){
+		return 7 + 8;
+	}
+}
+	
+	
+
+
